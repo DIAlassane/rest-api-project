7037e55f96cc399a8ff7147e2e9ba64fe35e05be
DIAlassane <alassdia0909@gmail.com>
2023-10-09 18:32:09 +0200

    deux

--- rest-api-app/src/App.js
+++ rest-api-app/src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Users from './component/users';
-import UpdateUser from './component/UpdateUser';
+import Users from './components/Users';
 import CreateUser from './components/CreateUser';
+import UpdateUser from './components/UpdateUser';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<Users/>}></Route>
-    <Route path='/update/:id' element={<UpdateUser/>}></Route>
-    <Route path='/createUser' element={<CreateUser/>}></Route>
-    
-   </Routes>
-   </BrowserRouter>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users />}></Route>
+        <Route path='/create' element={<CreateUser />}></Route>
+        <Route path='/update/:id' element={<UpdateUser />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
